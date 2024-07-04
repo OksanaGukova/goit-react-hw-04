10ed003c81481e3ca664060c0b3cb5c15b3821a1
OksanaGukova <kseniagukova@ukr.net>
2024-07-04 21:41:18 +0300

    update

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,20 +1,9 @@
 import css from './ImageCard.module.css'
 
-export default function ImageCard({
-  url,
-  description,
-  author,
-  likes,
-  onImageClick,
-}) {
+export default function ImageCard({ url, onImageClick }) {
   return (
     <div className={css.card} onClick={onImageClick}>
-      <img src={url} alt={description} className={css.image} />
-      <div className={css.info}>
-        <h3>{author}</h3>
-        <p>{description}</p>
-        <p>Likes: {likes}</p>
-      </div>
+      <img src={url} alt="" className={css.image} />
     </div>
   );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,34 +4,21 @@ import css from './ImageGallery.module.css'
 export default function ImageGallery({ photos, onPhotosClick }) {
   return (
     <ul className={css.container}>
-      {photos.map(
-        ({
-          id,
-          urls: { small, regular },
-          description,
-          user: { name, portfolio_url },
-          likes,
-        }) => (
-          <li className={css.photoContainer} key={id}>
-            <ImageCard
-              url={small}
-              description={description}
-              author={name}
-              likes={likes}
-              portfolio={portfolio_url}
-              onImageClick={() =>
-                onPhotosClick({
-                  url: regular,
-                  description,
-                  author: name,
-                  likes,
-                  portfolio: portfolio_url,
-                })
-              }
-            />
-          </li>
-        )
-      )}
+      {photos.map(({ id, urls: { small, regular }, description, user: { name }, likes }) => (
+        <li className={css.photoContainer} key={id}>
+          <ImageCard
+            url={small}
+            onImageClick={() =>
+              onPhotosClick({
+                url: regular,
+                description,
+                author: name,
+                likes,
+              })
+            }
+          />
+        </li>
+      ))}
     </ul>
   );
 }
